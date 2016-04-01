5953826f81d79611db148887a44d24d24d02f1d8
Aaron Sky <aaronsky@skyaaron.com>
2016-03-31 20:54:08 -0400

    Finished implementation of thermosensing logic for lights

--- pcomp_proj2.ino
+++ pcomp_proj2.ino
@@ -1,24 +1,25 @@
 #include <Adafruit_NeoPixel.h>
 
 const byte NEOPIXEL_PIN = 0;
-//const byte THERM_PIN = A0;
+const byte THERM_PIN = 1;
 
 Adafruit_NeoPixel pixel = Adafruit_NeoPixel(3, NEOPIXEL_PIN);
 
 void setup() {
   // put your setup code here, to run once:
-  //pinMode(THERM_PIN, INPUT);
   pixel.begin();
   //Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //int thermVal = analogRead(THERM_PIN);
+  int thermVal = analogRead(THERM_PIN);
   //Serial.println(thermVal);
-  for (int i = 0; i < 3; i++) {
-    pixel.setPixelColor(i, pixel.Color(0, 255, 0));
-    pixel.show();
+  if (thermVal <= 500) {
+    for (int i = 0; i < 3; i++) {
+      pixel.setPixelColor(i, pixel.Color(0, 255, 0));
+      pixel.show();
+    }
   }
   delay(500);
 }
